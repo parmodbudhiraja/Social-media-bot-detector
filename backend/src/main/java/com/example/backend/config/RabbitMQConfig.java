c268be3d9966bbb9629f65ab16b65a9a11fc9286
parmodbudhiraja02-design <parmodbudhiraja02@gmail.com>
2026-04-04 14:15:03 +0530

    Add Jackson2JsonMessageConverter to RabbitMQ to fix Java-Python serialization mismatch

--- backend/src/main/java/com/example/backend/config/RabbitMQConfig.java
+++ backend/src/main/java/com/example/backend/config/RabbitMQConfig.java
@@ -4,6 +4,10 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -39,5 +43,17 @@ public class RabbitMQConfig {
     @Bean
     public Binding resultsBinding(Queue resultsQueue, TopicExchange exchange) {
         return BindingBuilder.bind(resultsQueue).to(exchange).with(ROUTING_KEY_RESULTS);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+        final RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jsonMessageConverter());
+        return rabbitTemplate;
     }
 }
